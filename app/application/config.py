6b74ee933f2dab5c5e98e4310c2df8e2cc52914e
kymry burwell <kymry.burwell@gmail.com>
2020-02-04 19:39:38 -0800

    fixed old commit

--- app/application/config.py
+++ app/application/config.py
@@ -4,11 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    """ All configurations for the app """
-    DEBUG = True  # Turns on debugging features in Flask
-    MONGO_URI = "mongodb://127.0.0.1:27017/marsdata"  # Connects to MongoDB running on localhost on port 27017
+    DEBUG = True
+    MONGO_URI = "mongodb://127.0.0.1:27017/marsdata"
     SECRET_KEY = os.environ.get('SECRET_KEY') or "H49J*dE#4kslf"  # needed for Flask-WTF form security
     DATABASE = os.environ.get('DATABASE') or os.path.join(basedir, 'databases/astronomicaldata.db')
     # 'sqlite:///' tells SQLAlchemy which database engine to use
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'databases/astronomicaldata.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True  # if True, signals a change every time an update is made to the db
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
